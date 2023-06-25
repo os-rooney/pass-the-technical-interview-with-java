8683da54264080e0169f4ed6c826abf866a0195e
roosman <dev@ronios.de>
2023-06-25 18:36:58 +0200

    adjust Node class for DoublyLinkedList

--- Node.java
+++ Node.java
@@ -2,6 +2,7 @@ public class Node {
     
     public String data;
     private Node next;
+    private Node previous;
 
     public Node(String data) {
         this.data = data;
@@ -12,8 +13,16 @@ public class Node {
         this.next = node;
     }
 
+    public void setPreviousNode(Node node) {
+        this.previous = node;
+    }
+
     public Node getNextNode() {
         return this.next;
+    }
+
+    public Node getPreviousNode() {
+        return this.previous;
     }
 
     public static void main(String[] args) {
